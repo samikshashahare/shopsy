a079f1cade6b76bf5c398a885eae61bc78728981
samikshashahare <samikshashahare953@gmail.com>
2023-10-27 20:17:23 +0530

    created signup and login

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-import  express  from "express";
-import mongoose from 'mongoose';
-import axios from "axios";
+import  express  from "express"
+import mongoose from 'mongoose'
+import User from "./model/User.js";
 import dotenv from 'dotenv';
 dotenv.config();
+
 
 const app = express();
 app.use(express.json());
@@ -12,11 +13,77 @@ const connectDB = async()=>{
     if(conn){
         console.log("MongoDB connected");
     }
-}
+};
+connectDB();
+//POST /signup
+
+app.post("/signup", async (req, res) =>{
+    const {name, email, password, mobile, address, gender } = req.body;
+
+    const user = new User({
+        name: name,
+        email: email,
+        password:password,
+        mobile: mobile,
+        address: address,
+        gender: gender
+
+    });
+
+    try{
+        const saveduser = await user.save();
+
+        res.json({
+            success:true,
+            data: saveduser,
+            message:" signup  successfully"
+        });
+    }
+
+    catch(e){
+        res.json({
+            success:false,
+            data: saveduser,
+            message:" e.message"
+        });
+    }
+}) ;
+
+//POST/ login
+
+app.post("/login", async(req, res)=>{
+    const{email,password} = req.body;
+
+    if(!email || !password){
+        return res.json({
+            success:false,
+            message:"please provide email and password"
+        })
+    }
+
+    const user = await User.findOne({
+        email:email,
+        password:password
+    }).select("name email mobile")
+    if(user){
+        return res.json({
+            success:true,
+            data:user,
+            message:"Login successful"
+        });
+        
+    }
+    else{
+        return res.json({
+            success:false,
+            message:"Invalid credentials"
+        });
+    }
+})
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
     console.log(`server is runnung on port: ${PORT}`);
-    connectDB()
+   
 });
---
+++ server/model/User.js
@@ -0,0 +1,35 @@
+import { Schema, model} from "mongoose";
+
+const userSchema = new Schema({
+name: {
+    type:String,
+    default:"-"
+},
+email:{
+    type:String,
+    unique:true,
+    required:true
+},
+password:{
+    type:String,
+    required:true
+},
+mobile:{
+    type:String,
+    unique:true,
+    required:true
+},
+address:{
+    type:String,
+   
+},
+gender:{
+    type:String,
+    default:"prefer not to say"
+}
+
+});
+
+const User = model('User', userSchema);
+
+export default User;
