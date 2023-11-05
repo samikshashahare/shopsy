0d026afddcaa03a3bf46a966a7251fbf09ad6b12
samikshashahare <samikshashahare953@gmail.com>
2023-11-05 23:17:03 +0530

    signup page is created

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Login from './views/Login/Login';
+import Signup from './views/Signup/Signup';
+import "./index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  
+  // {
+  //   path:'',
+  //   element: <Home />
+  // },
+  {
+    path:'',
+    element: <Login />
+  },
+  {
+    path:'/signup',
+    element:<Signup />
+  }
+])
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<RouterProvider router={router}/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ client/src/views/Signup/Signup.js
@@ -0,0 +1,153 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Signup.css';
+
+
+function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [address, setAddress] = useState("");
+  const [gender, setGender] = useState("female");
+
+  const signup = async () => {
+    if (!name) {
+      alert("name is required");
+      return
+    }
+    if (!email) {
+      alert("email is required");
+      return
+    }
+    if (!password) {
+      alert("password is required");
+      return
+    }
+    if (!mobile) {
+      alert("mobile is required");
+      return
+    }
+    if (!address) {
+      alert("address is required");
+      return
+    }
+    const responce = await axios.post("/signup", {
+      name: name,
+      email: email,
+      password: password,
+      mobile: mobile,
+      address: address,
+      gender:gender
+    
+    })
+
+    alert(responce?.data?.message);
+
+
+if(responce?.data?.success){
+  window.location.href = "/login";
+}
+
+
+  };
+  return (
+    <div>
+      <form className='signup-form'>
+        <h1 className='text-center'>signup</h1>
+
+        <div>
+          <label htmlfor="name">  Name</label>
+          <input type='text'
+            className='form-control'
+            id="name"
+            placeholder='Enter name'
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }} />
+        </div>
+        <div>
+          <label htmlfor="email"> Email</label>
+          <input type='text'
+            className='form-control'
+            id="email"
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }} />
+        </div>
+        <div>
+          <label htmlfor="password">  Password</label>
+          <input type='password'
+            className='form-control'
+            id="password"
+            placeholder='Enter password'
+            hidden='true'
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }} />
+        </div>
+        <div>
+          <label htmlfor="mobile"> Enter Mobile</label>
+          <input type='text'
+            className='form-control'
+            id="mobile"
+            placeholder='Enter mobile'
+            value={mobile}
+            onChange={(e) => {
+              setMobile(e.target.value);
+            }} />
+        </div>
+        <div>
+          <label htmlfor="address"> Address</label>
+          <input type='text'
+            className='form-control'
+            id="address"
+            placeholder='Enter address'
+            value={address}
+            onChange={(e) => {
+              setAddress(e.target.value);
+            }} />
+        </div>
+        <div>
+
+          <input type='radio'
+            id='male'
+            name='gender'
+            className='gender'
+            checked={gender == "male"}
+            onClick={() => {
+              setGender("male");
+            }}
+          />
+          <label htmlfor="male">Male</label>
+
+
+          <input type='radio'
+            id='female'
+            name='gender'
+            className='gender'
+            checked={gender == "female"}
+            onClick={() => {
+              setGender("female")
+            }}
+          />
+          <label htmlfor="female" >Female</label>
+
+        </div>
+
+        <div>
+          <button type='button'
+            className='btn signup-btn'
+            onClick={signup}
+          >signup</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Signup
